6ed063aaf768a562b790f87a766b1122e306df8b
Sahbi <sahbisingh017@gmail.com>
2024-08-20 22:11:36 +0530

    Added codes

---
+++ index.js
@@ -0,0 +1,23 @@
+class product{
+    name;
+    prices;
+    category;
+    discription;
+    rating;
+
+        addToCart(){
+            console.log("Product added to cart");
+        }
+        removeFromCart() {
+            console.log("Product removed from cart");
+        }
+        displayProduct() {
+        console.log("Product displayed");
+        }
+         
+        buyProduct() {
+        console.log("Product bought");
+        }
+
+
+}
